8b7f9c09ae15329c7ac3364d8a164a60b15db6cd
walter j <jimenezjimenezw@gmail.com>
2023-03-29 22:55:32 -0600

    implementacion de logout

--- src/app/data/JwtResponseI.Interface.ts
+++ src/app/data/JwtResponseI.Interface.ts
@@ -3,4 +3,6 @@ import { ErrorResponseI } from "./ErrorResponseI.Interface";
 export interface JwtResponseI extends ErrorResponseI {
         jwtToken:string,
         expireAt:string
+        id:number,
+        username:string
 }
